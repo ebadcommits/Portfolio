7a7c244979ee7d566b1246604082b1fce0c36851
beestcode <153935496+beestcode@users.noreply.github.com>
2026-01-05 03:57:51 +0500

    portfolio done

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 function toggleMenu() {
     document.getElementById("menu").classList.toggle("show");
     document.querySelector(".menu-btn").style.display = "none";
-    document.getElementById("menu").style.display="block"
+    // document.getElementById("menu").style.display="block"
 }
     //hide menu
 function closeMenu() {
+    
     document.getElementById("menu").classList.remove("show");
     document.querySelector(".menu-btn").style.display = "block";
-    document.getElementById("menu").style.display="none"
+    // document.getElementById("menu").style.display="none"
 }
-document.querySelectorAll('#menu').forEach(link => {
-    link.addEventListener('click', closeMenu);
-});
+// document.querySelectorAll('#menu').forEach(link => {
+//     link.addEventListener('click', closeMenu);
+// });
+if (window.innerWidth < 768) {
+    document.querySelectorAll('#menu a').forEach(link => {
+        link.addEventListener('click', closeMenu);
+    });
+}
