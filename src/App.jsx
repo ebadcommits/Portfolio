859c6a29b84c13db3058d864214de6ccff383ea7
ebad sheikh <ibbeescode@gmail.com>
2026-09-10 20:27:06 +0500

    Add Advanced component and integrate it into the main App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Projects from './components/Projects'
+import Advanced from './components/Advanced'
 import Footer from './components/Footer'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="max-w-4xl mx-auto px-4 py-8"> 
         <Header />
         <Projects />
+        <Advanced />
         <Footer />
       </div>
     </div>
